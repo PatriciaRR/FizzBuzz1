db88123f5bd56246850f5ddeec258f9c487bbe61
Patricia Robles <patriciarobles9@gmail.com>
2022-04-26 15:06:25 +0100

    additional logic for multiples of 13

--- FizzBuzz1.js
+++ FizzBuzz1.js
@@ -4,11 +4,6 @@ function printStrings() {
     
     for (let i = 1; i <= 105; i++) {
         let displayWord = '';
-    //     if (i % 11 === 0) {
-    //         displayWord = 'Bong';
-    //         console.log(displayWord);
-    //         continue;
-    //    }
         if (i % 3 === 0) {
             displayWord += 'Fizz';
         }
@@ -20,6 +15,14 @@ function printStrings() {
         }
         if (i % 11 === 0) {
             displayWord = 'Bong';
+        }
+        if (i % 13 === 0) {
+            let indexOfB = displayWord.indexOf('B');
+            if (indexOfB === -1) { 
+                displayWord += 'Fezz'
+            } else { 
+                displayWord = displayWord.slice(0,indexOfB) + 'Fezz' + displayWord.slice(indexOfB);
+            }
         }
         if (displayWord === '') {
             displayWord += i;

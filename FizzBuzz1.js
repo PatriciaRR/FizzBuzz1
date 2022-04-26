0d4c21e5418ba5c75485346d731d959f529cc822
Patricia Robles <patriciarobles9@gmail.com>
2022-04-26 12:09:25 +0100

    simplified conditional logic using concatenation

--- FizzBuzz1.js
+++ FizzBuzz1.js
@@ -1,16 +1,20 @@
 console.log('Hello world!');
 
 function printNumbers () {
+    let displayWord = '';
     for (let i = 1; i <= 100; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz")
-        } else if (i % 3 === 0) {
-            console.log("Fizz")
-        } else if (i % 5 === 0) {
-            console.log("Buzz")
-        } else {
-            console.log(i);
-        }  
+        if (i % 3 === 0) {
+            displayWord = 'Fizz';
+        }
+        if (i % 5 === 0) {
+            displayWord += 'Buzz';
+        }
+        else {
+            displayWord = i;
+        }
     }
+    return console.log(displayWord);
 }
+
 printNumbers();
+

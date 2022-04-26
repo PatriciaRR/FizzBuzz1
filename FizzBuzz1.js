cf4d743c89e07e0c2f644778cbce0de791302582
Patricia Robles <patriciarobles9@gmail.com>
2022-04-26 14:54:44 +0100

    additional logic added for multiples of 11

--- FizzBuzz1.js
+++ FizzBuzz1.js
@@ -4,6 +4,11 @@ function printStrings() {
     
     for (let i = 1; i <= 105; i++) {
         let displayWord = '';
+    //     if (i % 11 === 0) {
+    //         displayWord = 'Bong';
+    //         console.log(displayWord);
+    //         continue;
+    //    }
         if (i % 3 === 0) {
             displayWord += 'Fizz';
         }
@@ -11,7 +16,10 @@ function printStrings() {
             displayWord += 'Buzz';
         }
         if (i % 7 === 0) {
-            displayWord += 'Bang';
+            displayWord += 'Bang';    
+        }
+        if (i % 11 === 0) {
+            displayWord = 'Bong';
         }
         if (displayWord === '') {
             displayWord += i;

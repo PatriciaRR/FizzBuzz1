0f7298e9853497117fb7d5100b574ca6afbc7807
Patricia Robles <patriciarobles9@gmail.com>
2022-04-26 13:48:11 +0100

    additional logic for multiples of 7

--- FizzBuzz1.js
+++ FizzBuzz1.js
@@ -1,8 +1,8 @@
 console.log('Hello world!');
 
-function printNumbers() {
+function printStrings() {
     
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 105; i++) {
         let displayWord = '';
         if (i % 3 === 0) {
             displayWord += 'Fizz';
@@ -10,11 +10,14 @@ function printNumbers() {
         if (i % 5 === 0) {
             displayWord += 'Buzz';
         }
+        if (i % 7 === 0) {
+            displayWord += 'Bang';
+        }
         if (displayWord === '') {
             displayWord += i;
         }
         console.log(displayWord);
     } 
 }
-printNumbers();
+printStrings();
 
